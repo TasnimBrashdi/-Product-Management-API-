e087b1cd1eeb5fa56656abc4d0d8c492bc6108ac
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-12 13:19:40 +0400

    Add interface repo

---
+++ End Point Parameters Task/Repositories/IProductRepo.cs
@@ -0,0 +1,13 @@
+﻿using End_Point_Parameters_Task.Models;
+
+namespace End_Point_Parameters_Task.Repositories
+{
+    public interface IProductRepo
+    {
+        void Add(Product product);
+        void Delete(int id);
+        IQueryable<Product> GetAll();
+        Product GetById(int id);
+        void Update(Product product);
+    }
+}
