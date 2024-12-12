e19a2a711b0b12fd1d00cf8ac34e2561e93f1d62
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-12 12:24:50 +0400

    Add Model Product

--- End Point Parameters Task/Models/Product.cs
+++ End Point Parameters Task/Models/Product.cs
@@ -1,6 +1,23 @@
-﻿namespace End_Point_Parameters_Task.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace End_Point_Parameters_Task.Models
 {
     public class Product
     {
+        [Key]
+        public int PID { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public decimal Price { get; set; }
+        [Required]
+        public string Category { get; set;} = "general";
+        [Required]
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+
     }
 }
