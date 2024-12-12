1c19673810d198f13c0a7b0ca91d2d53f6ee13ca
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-12 12:47:09 +0400

    Add DBContext

---
+++ End Point Parameters Task/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using End_Point_Parameters_Task.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace End_Point_Parameters_Task
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
