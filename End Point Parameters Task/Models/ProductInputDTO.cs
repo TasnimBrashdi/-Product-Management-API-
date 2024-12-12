b519b905fc302026288cd8244af02b9eb89c1368
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-12 12:31:19 +0400

    Add Model Product DTO

---
+++ End Point Parameters Task/Models/ProductInputDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace End_Point_Parameters_Task.Models
+{
+    public class ProductInputDTO
+    {
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = "general"; 
+
+    }
+}
