333676d07381efc6a07c0ccbe55e39dec257a0af
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-12 12:43:19 +0400

    Add Product Output Model

---
+++ End Point Parameters Task/Models/ProductOutputDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace End_Point_Parameters_Task.Models
+{
+    public class ProductOutputDTO
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
