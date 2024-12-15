137dd793c962aca103d89958d4bc64dd14535c63
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-15 15:12:25 +0400

    Add User service and interface

---
+++ End Point Parameters Task/Sevices/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using End_Point_Parameters_Task.Models;
+
+namespace End_Point_Parameters_Task.Sevices
+{
+    public interface IUserService
+    {
+        void AddUser(User user);
+        User GetUser(string email, string password);
+    }
+}
---
+++ End Point Parameters Task/Sevices/UserService.cs
@@ -0,0 +1,28 @@
+﻿using End_Point_Parameters_Task.Models;
+using End_Point_Parameters_Task.Repositories;
+
+namespace End_Point_Parameters_Task.Sevices
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepo _userrepo;
+
+        public UserService(IUserRepo userrepo)
+        {
+            _userrepo = userrepo;
+        }
+
+
+        public void AddUser(User user)
+        {
+
+            _userrepo.AddUser(user);
+        }
+
+        public User GetUser(string email, string password)
+        {
+            return _userrepo.GetUSer(email, password);
+
+        }
+    }
+}
