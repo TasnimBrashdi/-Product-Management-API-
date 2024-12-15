84cd2e6c62a9c636153d536ab888041ff75e3723
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-15 14:57:14 +0400

    Add User Model

---
+++ End Point Parameters Task/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace End_Point_Parameters_Task.Models
+{
+    public class User
+    {
+        [Key]
+        [JsonIgnore]
+        public int UId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
