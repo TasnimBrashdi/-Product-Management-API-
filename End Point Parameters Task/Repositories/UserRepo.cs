7be5f748b11bd50ab3b90bb4fd7f992a333ab8a6
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-15 15:02:19 +0400

    Add user repository

---
+++ End Point Parameters Task/Repositories/UserRepo.cs
@@ -0,0 +1,25 @@
+﻿using End_Point_Parameters_Task.Models;
+
+namespace End_Point_Parameters_Task.Repositories
+{
+    public class UserRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public User GetUSer(string email, string password)
+        {
+            return _context.Users.Where(u => u.Email == email & u.Password == password).FirstOrDefault();
+        }
+    }
+}
